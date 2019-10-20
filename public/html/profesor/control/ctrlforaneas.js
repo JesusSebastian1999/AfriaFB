e666df21d052b6316856b9e11fb88be0c7e6a527
JesusSebastian1999 <SebasUTN12@hotmail.com>
2019-10-19 19:31:33 -0500

    borrados

--- public/html/profesor/control/ctrlforaneas.js
+++
@@ -1,8 +0,0 @@
-export async function carga_grupos() {
- const querySnapshot = await firebase.firestore().collection("GRUPOS")
-   .orderBy("GRUPO_NOMBRE").get();
- document.vista.grupo.cargada();
- document.vista.grupo.agrega({ id: "", texto: "Sin grupo" });
- querySnapshot.forEach(doc => document.vista.grupo.agrega(
-   { id: doc.id, texto: doc.data().GRUPO_NOMBRE }));
-}
